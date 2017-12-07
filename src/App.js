21c8299da14bffea715a77c8f76d7e6439b6b274
Hervin Efrain Garcia Cardona <hervin_rambito@hotmail.com>
2017-12-07 17:11:08 +0000

    create CSS for my components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Menu from './Components/menu/Menu.js';
+
 import './App.css';
 
 class App extends Component {
     render() {
-        return ( < div >
-            <
-            Menu / >
-            <
-            /div>
-        );
+        return () { <
+            div >
+                <
+                menu / >
+                <
+                /div>
+        }
     }
 }
 
--- src/Components/menu/Menu.js
+++ src/Components/menu/Menu.js
@@ -1,44 +1,39 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import { Link,nav } from "react-router-dom";
+import { Link, nav } from "react-router-dom";
 import './Menu.css';
 
 class Menu extends Component {
 
-  constructor() {
-  	super();
-    this.state = { flipped: null, showIcons: false };
-  }
+    constructor() {
+        super();
+        this.state = { flipped: null, showIcons: false };
+    }
 
-  mouseOut() {
-    console.log("Mouse out!!!");
-    this.setState({flipped: false});
-  }
-  
-  mouseOver() {
-    console.log("Mouse over!!!");
-    this.setState({flipped: true});
-  }
-onHover(){
-  this.setState({showIcons: true})
-}
 
-  
-  render() {
-  	return (
-      <div>
-      <h1 className="menu">
-        <ul onMouseOver = {this.onHover}>
-          <li><img src="/icons/menu-menu.png" alt="" /></li>
-          <li><Link to="/Layout"><img src="/icons/menu-setting.png" alt="" /></Link></li>
-          <li><Link to="/Footer"><img src="/icons/menu-login.png" alt="" /></Link></li>
-          <li><Link to="/About"><img src="/icons/menu-info.png" alt="" /></Link></li>
-      </ul>
-      </h1>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div >
+            <
+            h1 className = "menu" >
+            <
+            ul >
+            <
+            li > < img src = "/icons/menu-menu.png"
+            alt = "" / > < /li> <
+            li > < Link to = "/Layout" > < img src = "/icons/menu-setting.png"
+            alt = "" / > < /Link></li >
+            <
+            li > < Link to = "/Footer" > < img src = "/icons/menu-login.png"
+            alt = "" / > < /Link></li >
+            <
+            li > < Link to = "/About" > < img src = "/icons/menu-info.png"
+            alt = "" / > < /Link></li >
+            <
+            /ul> <
+            /h1> <
+            /div>
+        );
+    }
 }
 
 
